7ebadbcbbd1fbb27e518b805380312b331f9e48f
ProCoder <109970325+heckerweb@users.noreply.github.com>
2022-08-04 20:22:25 +0700

    Create random.py

---
+++ random.py
@@ -0,0 +1,6 @@
+import calendar
+import random
+print ("						[ Random Calendar ]")
+print('==========================================================================')
+cal = random.randint(2022,2050)
+print (calendar.calendar(cal))
